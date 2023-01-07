ac79f4cff3f53fe7294714b86e56b230b47b59a9
Phil Whittaker <phil@hifi.agency>
2023-01-07 09:19:44 +0000

    add central file manager

--- post-install.js
+++ post-install.js
@@ -1,5 +1,4 @@
-const fs = require('fs');
-const path = require('path');
+const core = require('yuzu-plugin-core');
 const files = [ 
     {
         source: './_dev/_templates/blocks/gridBuilder/parGridBuilderItems.schema',
@@ -21,25 +20,4 @@ const files = [
 
 console.log(`Yuzu Definition Grid Plugin PostInstall`);
 
-files.forEach((file) => {
-    if(fs.existsSync('../../package.json')) {
-        let destPath = path.dirname(file.dest);
-        if(fs.existsSync(destPath) && fs.existsSync(file.dest)) {
-            console.log(`${file.dest} already installed, not overwriting`);
-            //we have to delete files that have been overwritten in the child site, otherwise we get nasty clashes
-            if(fs.existsSync(file.source)) {
-                fs.rmSync(file.source);
-            }
-        }
-        else if(!fs.existsSync(file.source))  {
-            console.log(`${file.source}, source doesn't exist`);
-        }
-        else {
-            console.log(`Installing file to ${file.dest}`);
-            if(!fs.existsSync(destPath)) {
-                fs.mkdirSync(destPath, { recursive: true })
-            }
-            fs.renameSync(file.source, file.dest);
-        }
-    }
-})
+core.postInstallManageFiles(files);
